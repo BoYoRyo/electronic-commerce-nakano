56f52ce565285904b97b5b377bf05641401219d6
BoYoRyo <nakanor455@gmail.com>
2022-09-08 15:37:16 +0900

    ユーザー登録のサービスクラスを作成しました

---
+++ src/main/java/com/example/service/UserService.java
@@ -0,0 +1,26 @@
+package com.example.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.domain.User;
+import com.example.repository.UserRepository;
+
+/**
+ * usersテーブルのサービス機能を行うクラス.
+ * 
+ * @author nakanoryo
+ *
+ */
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+	
+	/**
+	 * 新規ユーザー登録を行う.
+	 * 
+	 * @param user user
+	 */
+	public void userRegistration(User user) {
+		userRepository.insertUser(user);
+	}
+}
