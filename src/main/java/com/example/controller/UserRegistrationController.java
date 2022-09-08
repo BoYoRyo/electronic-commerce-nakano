a13ef15ebb9931bd82eb11bed287c5cb89856bbd
BoYoRyo <nakanor455@gmail.com>
2022-09-08 16:36:01 +0900

    ログイン画面に遷移するメソッド追加しました

--- src/main/java/com/example/controller/UserRegistrationController.java
+++ src/main/java/com/example/controller/UserRegistrationController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -36,6 +37,16 @@ public class UserRegistrationController {
 		user.setName(name);
 		userRegistrationService.userRegistration(user);
 		return "redirect:/toLogin";
+	}
+	
+	/**
+	 * ログイン画面に遷移.
+	 * 
+	 * @return　ログイン画面
+	 */
+	@GetMapping("/toLogin")
+	public String toLogin() {
+		return "login";
 	}
 
 }
