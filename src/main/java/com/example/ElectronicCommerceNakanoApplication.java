4b97ce55ad031d49dbef19c30e686738231a42c5
BoYoRyo <nakanor455@gmail.com>
2022-09-08 10:50:57 +0900

    first commit

---
+++ src/main/java/com/example/ElectronicCommerceNakanoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicCommerceNakanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicCommerceNakanoApplication.class, args);
+	}
+
+}
