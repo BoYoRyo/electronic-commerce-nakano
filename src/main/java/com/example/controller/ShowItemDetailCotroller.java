86ccc2b0f7a53460a5025e969e6ab5b3aed10d20
BoYoRyo <nakanor455@gmail.com>
2022-09-09 17:03:54 +0900

    toppingリストを商品詳細画面に表示できるようにしました

--- src/main/java/com/example/controller/ShowItemDetailCotroller.java
+++ src/main/java/com/example/controller/ShowItemDetailCotroller.java
@@ -1,5 +1,7 @@
 package com.example.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,7 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Item;
+import com.example.domain.Topping;
 import com.example.service.ShowItemDetailService;
+import com.example.service.ShowToppingListService;
 
 /**
  * 商品詳細画面に遷移する.
@@ -20,6 +24,9 @@ import com.example.service.ShowItemDetailService;
 public class ShowItemDetailCotroller {
 	@Autowired
 	private ShowItemDetailService showItemDetailService;
+	
+	@Autowired
+	private ShowToppingListService showToppingListService;
 
 	/**
 	 * 詳細画面へ遷移.
@@ -32,6 +39,8 @@ public class ShowItemDetailCotroller {
 	public String showItemDetail(Integer id, Model model) {
 		Item item = showItemDetailService.showItemDetail(id);
 		model.addAttribute("item", item);
+		List<Topping> toppingList = showToppingListService.ShowAllTopping();
+		model.addAttribute("toppingList", toppingList);
 		return "item_detail";
 	}
 }
